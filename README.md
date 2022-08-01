a207339948818d39294a36436f494fdcd26e4565
Andrea <andrea.juarez@gmail.com>
2022-08-01 02:01:29 +0000

    Ejercicio

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Template for Machine Learning projects
+# UNSUPERVISED LEARNING
+House clustering
+
+We will create 6 housing clusters based only on their 'latitude','longitude' and their 'medincome' column.
+
+Dataset links:
+
+https://raw.githubusercontent.com/4GeeksAcademy/k-means-project-tutorial/main/housing.csv
--- src/app.py
+++ src/app.py
@@ -1,4 +1,21 @@
-from utils import db_connect
-engine = db_connect()
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
-# your code here
+from sklearn import decomposition
+from sklearn import datasets
+from sklearn.cluster import KMeans
+
+from scipy.cluster.vq import kmeans, vq
+
+df_raw = pd.read_csv('https://raw.githubusercontent.com/4GeeksAcademy/k-means-project-tutorial/main/housing.csv')
+
+X = df_raw.loc[:, ["MedInc", "Latitude", "Longitude"]]
+
+kmeans = KMeans(n_clusters=6)
+X["Cluster"] = kmeans.fit_predict(X)
+X["Cluster"] = X["Cluster"].astype("category")
+
+sns.relplot(
+    x="Longitude", y="Latitude", hue="Cluster", data=X, height=6,
+);
